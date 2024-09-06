8bcb6171c048479fe87f2fd114411f9858c9c802
Mroziuk Magdalena (BG) <mmroziuk@bzmw.gov.pl>
2024-09-06 10:11:46 +0200

    widok mapy narzedzia skecja komponent pusty

---
+++ src/app/modul-mapowy/serwis-mapowy/komponenty/mapa/widok-mapy-narzedzia-sekcja/widok-mapy-narzedzia-sekcja.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mm-widok-mapy-narzedzia-sekcja',
+  templateUrl: './widok-mapy-narzedzia-sekcja.component.html',
+  styleUrls: ['./widok-mapy-narzedzia-sekcja.component.scss']
+})
+export class WidokMapyNarzedziaSekcjaComponent {
+
+}
