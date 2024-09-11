99a0a523381ea13b77507b46b5e2a89fd3c1bd40
Mroziuk Magdalena (BG) <mmroziuk@bzmw.gov.pl>
2024-09-11 08:48:14 +0200

    belka-boczna-kontener utorzony

---
+++ src/app/modul-mapowy/serwis-mapowy/komponenty/boczna-belka/boczna-belka-kontener/boczna-belka-kontener.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boczna-belka-kontener',
+  templateUrl: './boczna-belka-kontener.component.html',
+  styleUrls: ['./boczna-belka-kontener.component.scss']
+})
+export class BocznaBelkaKontenerComponent {
+
+}
