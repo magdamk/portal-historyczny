e9e14e33096c7ddef7474aa72fd6a38eb98cfaca
Mroziuk Magdalena (BG) <mmroziuk@bzmw.gov.pl>
2024-10-07 14:19:15 +0200

    belka - mapy, plany

--- src/app/modul-mapowy/serwis-mapowy/komponenty/boczna-belka/os-mapy-plany/os-mapy-plany.component.ts
+++ src/app/modul-mapowy/serwis-mapowy/komponenty/boczna-belka/os-mapy-plany/os-mapy-plany.component.ts
@@ -10,6 +10,8 @@ import { KategoriaGrupaMapOpenDto } from 'src/app/core/modele/kategoria-grupa-ma
 import { TlumaczeniaService } from 'src/app/core/tlumaczenia/serwisy/tlumaczenia.service';
 import { KonfiguracjaModulMapowyAdapter } from 'src/app/modul-mapowy/mm-core/providers/konfiguracja-adapter';
 import { LewyPanelWidokActions } from 'src/app/modul-mapowy/stan/lewy-panel-widok/lewy-panel-widok.actions';
+import { Router } from '@angular/router';
+import { TypMapyObiektDto } from 'src/app/core/modele/typ-mapy-obiekt-dto';
 
 @Component({
   selector: 'mm-os-mapy-plany',
@@ -18,43 +20,90 @@ import { LewyPanelWidokActions } from 'src/app/modul-mapowy/stan/lewy-panel-wido
 })
 export class OsMapyPlanyComponent implements OnInit, OnDestroy {
   widokIdentyfikator = WIDOKI_ID.MAPY_PLANY;
-  @Input() widok?: Widok;
+  @Input() widok!: Widok;
   @Input() mapa?: Mapa;
   @Input() obszarWidoczny?: boolean | undefined;
   @Output() mapaWybrana = new EventEmitter<WyborMapyEvent>();
 
-  // zbiorMapPlanow: Array<KategoriaGrupaMapOpenDto>=[];
-  aktualnyJezyk = 'pl';
+  zbiorMapPlanow: Array<KategoriaGrupaMapOpenDto> = [];
+  aktualnyJezyk='';
   subscription$ = new Subscription();
   /**
    * Konstruktor
    */
   constructor(private tlumaczenia: TlumaczeniaService,
-    // private serviceKategoriiMap: ControllerKategorieMapService,
-    // private konfiguracja: KonfiguracjaModulMapowyAdapter,
-    private store: Store<{ modulMapowy: any }>) {
+    private serviceKategoriiMap: ControllerKategorieMapService,
+    private konfiguracja: KonfiguracjaModulMapowyAdapter,
+    private store: Store<{ modulMapowy: any }>, private router: Router) {
   }
 
-  ngOnInit() {
-    this.subscription$.add(this.tlumaczenia.getZmianaJezykaSubject().subscribe(jezyk => {
-      this.aktualnyJezyk = jezyk;
-    }));
-  }
+ /**
+  * Cykl życia komponentu inicjalizacja
+  */
+ ngOnInit(): void {
+  // // this.pobierzListeTematow();
+  // this.subscription$.add(this.tlumaczenia.getZmianaJezykaSubject()
+  //   .subscribe(() => this.pobierzListeTematow()));
+
+  this.subscription$.add(this.tlumaczenia.getZmianaJezykaSubject().subscribe(jezyk => {
+    this.aktualnyJezyk = jezyk;
+    this.pobierzListeMapPLanow();
+  }));
+  // this.subscription$.add(this.obszary$.subscribe(stan => {
+  //   this.obszarySterujace = stan.obszarySterujace.filter(n => n.wirtualne === false);
+  //   // this.przygotujListeNarzedziWarstw(stan.narzedziaSterujace)
+  // }));
+}
 
 
-  /**
-   * Cykl życia komponentu niszczenie
-   */
-  ngOnDestroy(): void {
-    this.subscription$.unsubscribe();
-  }
+/**
+ * Cykl życia komponentu niszczenie
+ */
+ngOnDestroy(): void {
+  this.subscription$.unsubscribe();
+}
 
-  /**
-    * Funkcja przenosi narzędzie na wierzch
-    */
-  przeniesNaWierzch(): void {
-    this.store.dispatch(LewyPanelWidokActions.pokazObszar({ widokId: this.widokIdentyfikator }));
-    // this.pobierzObszarySerwis().dispatch(InterfejsUzytkownikaActions.rozwinLewaBelke());
+/**
+* Funkcja przenosi narzędzie na wierzch
+*/
+przeniesNaWierzch(): void {
+  this.store.dispatch(LewyPanelWidokActions.pokazObszar({ widokId: this.widokIdentyfikator }));
+  // this.pobierzObszarySerwis().dispatch(InterfejsUzytkownikaActions.rozwinLewaBelke());
+}
+
+
+/**
+* Funkcja do pobierania listy kategorii map
+*/
+private pobierzListeMapPLanow(): void {
+
+  this.serviceKategoriiMap.getKategorieMap()
+    .subscribe((result: any) => {
+      console.log('pobierzListeKategoriiMap: ', result.content.kategorieTematyczne[1].grupyMap);
+      // if (result.content.typ.ObiektEnum==='KATEGORIA_TEMATYCZNA') {
+      this.zbiorMapPlanow = Array.from(result.content.kategorieTematyczne[1].grupyMap);
+      console.log('!!!!pobierzListeKategoriiMap: ', this.zbiorMapPlanow);
+      // }
+    });
+  // this.serviceKategoriiMap.pobierzListeKategorieMapDlaPortalu(wersja)
+  //   .subscribe((result: any) => {
+  //     if (result.content) {
+  //       this.zbiorKategoriiMap = result.content;
+  //     }
+  //   });
+  // this.zbiorTematow=[];
+}
+
+
+/**
+* Funkcja sygnalizuje wybraniwMapy
+*/
+wybranoMape(event: WyborMapyEvent): void {
+  if (event.typ === TypMapyObiektDto.ObiektEnumEnum.SerwisZewnetrzny) {
+    // this.komunikaty.pokazKomunikatBledu('codes.narzedzie-porownywania-map.blad-wyboru-mapy-komunikat', {});
+    return;
   }
+  // this.mapaWybrana.emit(event);
+}
 
 }
