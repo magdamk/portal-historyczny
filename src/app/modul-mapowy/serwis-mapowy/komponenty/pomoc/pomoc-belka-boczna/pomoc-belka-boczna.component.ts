68400405b0d765bc01a08af9cd06cee712346799
Mroziuk Magdalena (BG) <mmroziuk@bzmw.gov.pl>
2024-10-24 12:14:56 +0200

    szkielety komponentów pomocy

---
+++ src/app/modul-mapowy/serwis-mapowy/komponenty/pomoc/pomoc-belka-boczna/pomoc-belka-boczna.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mm-pomoc-belka-boczna',
+  templateUrl: './pomoc-belka-boczna.component.html',
+  styleUrls: ['./pomoc-belka-boczna.component.scss']
+})
+export class PomocBelkaBocznaComponent {
+
+}
---
+++ src/app/modul-mapowy/serwis-mapowy/komponenty/pomoc/pomoc-dialog/pomoc-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mm-pomoc-dialog',
+  templateUrl: './pomoc-dialog.component.html',
+  styleUrls: ['./pomoc-dialog.component.scss']
+})
+export class PomocDialogComponent {
+
+}
