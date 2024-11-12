90285e99c07b5f00dd8de96f585be9c6cd639500
Mroziuk Magdalena (BG) <mmroziuk@bzmw.gov.pl>
2024-11-12 12:53:58 +0100

    map-routing - wykasowanie nieużywanego importu

--- src/app/funkcjonalnosci/map/map-routing.module.ts
+++ src/app/funkcjonalnosci/map/map-routing.module.ts
@@ -1,10 +1,9 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {provideRouter, RouterModule, Routes, withDebugTracing, withHashLocation} from '@angular/router';
 import {StronaMapyComponent} from './strony/strona-mapy/strona-mapy.component';
 import {SzablonMapaComponent} from "../../core/szablony/szablon-mapa/szablon-mapa.component";
 // import {StronaMapyGuard} from "./guards/strona-mapy.guard";
 import {StronaPodgladuMapyComponent} from "./strony/strona-podgladu-mapy/strona-podgladu-mapy.component";
-import { BelkaKartaMapyComponent } from 'src/app/modul-mapowy/serwis-mapowy/komponenty/boczna-belka/belka-karta-mapy/belka-karta-mapy.component';
 
 // loadChildren: () => import('./funkcjonalnosci/komunikaty/komunikaty.module').then((m) => m.KomunikatyModule)
 const routes: Routes = [
@@ -20,8 +19,8 @@ const routes: Routes = [
     path: '',
     component: SzablonMapaComponent,
     children: [
-      {path: '', component: StronaMapyComponent,pathMatch: 'full',},
-      {path: ':uuid', component: StronaMapyComponent,pathMatch: 'full',}
+      {path: '', component: StronaMapyComponent},
+      {path: ':uuid', component: StronaMapyComponent}
     ],
   },
 ];
@@ -31,7 +30,10 @@ const routes: Routes = [
  */
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideRouter(routes, withHashLocation())
+  ]
 })
 export class MapRoutingModule {
 }
